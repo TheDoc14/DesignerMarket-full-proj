c66f842e7f50c52bc4bebb3bd2ecb64089c1ce70
Zeev <zeev5002@gmail.com>
2025-11-10 16:04:51 +0200

    fix

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -14,6 +14,8 @@ import AdminApprovalPage from './pages/AdminApprovalPage';
 import CreateProjectPage from './pages/CreateProjectPage';
 import EditProfilePage from './pages/EditProfilePage';
 import GlobalStyles from './styles/GlobalStyles';
+import PublicRoute from './components/guards/PublicRoute';
+
 
 
 
