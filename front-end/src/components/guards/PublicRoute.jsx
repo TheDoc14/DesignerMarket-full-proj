01e9dc38771bf669d09f6eb915b0b3fe98f156a1
Zeev <zeev5002@gmail.com>
2025-12-15 11:15:50 +0200

    Reset frontend and initialize with CRA

--- front-end/src/components/guards/PublicRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-function PublicRoute({ user, children }) {
-  return user ? <Navigate to="/profile" replace /> : children;
-}
-
-export default PublicRoute;
